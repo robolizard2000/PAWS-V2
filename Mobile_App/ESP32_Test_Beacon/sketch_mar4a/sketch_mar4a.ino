73b60cf4dc733c3bb25c2a75a0dd3136b771ee3c
robolizard2000 <robolizard2000@gmail.com>
2026-03-04 22:23:31 +0000

    first contact

---
+++ Mobile_App/ESP32_Test_Beacon/sketch_mar4a/sketch_mar4a.ino
@@ -0,0 +1,47 @@
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+
+#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+BLECharacteristic *pCharacteristic;
+bool deviceConnected = false;
+
+class MyServerCallbacks: public BLEServerCallbacks {
+  void onConnect(BLEServer* pServer) {
+    deviceConnected = true;
+  }
+
+  void onDisconnect(BLEServer* pServer) {
+    deviceConnected = false;
+  }
+};
+
+void setup() {
+  Serial.begin(115200);
+
+  BLEDevice::init("ESP32_BLE_Test");
+  BLEServer *pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  pCharacteristic = pService->createCharacteristic(
+                      CHARACTERISTIC_UUID,
+                      BLECharacteristic::PROPERTY_READ |
+                      BLECharacteristic::PROPERTY_WRITE
+                    );
+
+  pCharacteristic->setValue("Hello BLE");
+  pService->start();
+
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->start();
+
+  Serial.println("BLE device is ready and advertising...");
+}
+
+void loop() {
+}
--- Mobile_App/first-test/main.py
+++ Mobile_App/first-test/main.py
@@ -1,9 +1,12 @@
 import kivy                             # main kivy module
+from kivy.app import App                 # for creating the app class
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label        # for displaying text
+from kivy.uix.button import Button      # for creating buttons
 
-kivy.require('1.9.0')                   # specify the required kivy version   
+kivy.require('2.3.1')                   # specify the required kivy version   
 
-from BLE_Backend.ble_Manage import ble  # import the BLE backend manager
+from BLE_Backend.ble_Manage import ble  # import the BLE backend manager to allow for linux dev
 import asyncio
 
 async def main():
@@ -11,6 +14,28 @@ async def main():
     devices = await ble.scan()
     print("Scan results:", devices)
 
+#####
+
+class connect_BLE_Screen(GridLayout):
+    def __init__(self, **kwargs):
+        super(connect_BLE_Screen, self).__init__(**kwargs)
+        #self.cols = 1
+        #self.add_widget(Label(text="BLE Connection Screen"))
+        #self.add_widget(Button(text="Scan for Devices", on_press=self.Scan_Button_Pressed()))
+
+        self.devices = []
+
+    async def scan_for_devices(self): # creates an async function to perform the BLE scan and store the results in self.devices
+        self.devices = await ble.scan()
+
+    def Scan_Button_Pressed(self):
+        asyncio.run(self.scan_for_devices())
+        print("Scan results:", self.devices)
+
+class PAWSApp(App):
+    def build(self):
+        return connect_BLE_Screen()
+
 # run the async main function
 if __name__ == "__main__":
-    asyncio.run(main())
+    PAWSApp().run()
